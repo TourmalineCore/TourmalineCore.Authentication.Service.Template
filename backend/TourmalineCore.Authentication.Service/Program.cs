0d562296e9d78dc6fd40e36aee9a503edb8fa02d
Oleg Kl <oklyavlin@tourmalinecore.com>
2022-10-26 13:32:50 +0500

    build: add  configuration builder and logger builder

--- backend/TourmalineCore.Authentication.Service/Program.cs
+++ backend/TourmalineCore.Authentication.Service/Program.cs
@@ -1,7 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
 using Data;
 using Data.Models;
 using Data.Queries;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.EventLog;
 using TourmalineCore.AspNetCore.JwtAuthentication.Core;
 using TourmalineCore.AspNetCore.JwtAuthentication.Core.Models.Request;
 using TourmalineCore.AspNetCore.JwtAuthentication.Core.Options;
@@ -19,6 +22,65 @@ builder.Services.AddCors();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    var env = hostingContext.HostingEnvironment;
+
+    var reloadOnChange = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", true);
+
+    config.AddJsonFile("appsettings.json", true, reloadOnChange)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, reloadOnChange)
+        .AddJsonFile("appsettings.Active.json", true, reloadOnChange);
+
+    if (env.IsDevelopment() && !string.IsNullOrEmpty(env.ApplicationName))
+    {
+        var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+
+        config.AddUserSecrets(appAssembly, true);
+    }
+
+    config.AddEnvironmentVariables();
+
+    if (args != null)
+    {
+        config.AddCommandLine(args);
+    }
+
+});
+
+builder.Host.ConfigureLogging((hostingContext, logging) =>
+{
+    var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+    // IMPORTANT: This needs to be added *before* configuration is loaded, this lets
+    // the defaults be overridden by the configuration.
+    if (isWindows)
+    {
+        // Default the EventLogLoggerProvider to warning or above
+        logging.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Warning);
+    }
+
+    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    logging.AddConsole();
+    logging.AddDebug();
+    logging.AddEventSourceLogger();
+
+    if (isWindows)
+    {
+        // Add the EventLogLoggerProvider on windows machines
+        logging.AddEventLog();
+    }
+
+    logging.Configure(options =>
+    {
+        options.ActivityTrackingOptions = ActivityTrackingOptions.SpanId
+                                          | ActivityTrackingOptions.TraceId
+                                          | ActivityTrackingOptions.ParentId;
+    }
+        );
+
+});
 
 if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") != "Tests")
     builder
